1b107918e181228a7f6672d9f1a6329bb3c7dfca
Sumixraj <Sumixraj@gmail.com>
2026-02-16 14:35:59 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+hobby = input("What is your favourite hobby? ")
+university = input("What is your university name? ")
+department = input("What is your department name? ")
+stream = input("What is your stream? ")
+
+print("\n--- Student Profile ---")
+
+print(f"Welcome {name}!")
+print(f"You are {age} years old and you love {hobby}.")
+print(f"Your university name is {university}.")
+print(f"Your department and stream are {department} and {stream}.")
